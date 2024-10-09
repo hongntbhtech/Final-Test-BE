82a31233a257b5e540b99aba81e2cef3ff6443bf
hongntbhtech <hongnt.bhsoft@gmail.com>
2024-10-09 17:31:20 +0700

    abcde

--- src/routes/routers.js
+++ src/routes/routers.js
@@ -2,7 +2,7 @@ const authRoute = require('./auth.route');
 
 class Routes {
     constructor(app) {
-        app.use('/api/auth', authRoute); // Route for server
+        app.use('/api/auth', authRoute); 
     }
 }
 module.exports = Routes;
